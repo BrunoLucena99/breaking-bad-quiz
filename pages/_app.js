fdf0f56745b2e958d3a36a377780d28d3c076f38
BrunoLucena99 <brunohenriquelucena@gmail.com>
2021-01-31 16:08:14 -0300

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Breaking Bad Quiz</title>
+        <link rel="shortcut icon" href="https://static.thenounproject.com/png/191894-200.png" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
